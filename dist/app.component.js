53eb0bd54521eaa9d8975643a663336fc094493c
keiraeleison <keiraeleison@gmail.com>
2017-01-17 09:31:03 +0100

    Angular 2 starter app, introducing observables

--- dist/app.component.js
+++ dist/app.component.js
@@ -9,19 +9,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var http_1 = require("@angular/http");
 var AppComponent = (function () {
-    function AppComponent() {
-        this.message = 'This is the sample message.';
+    function AppComponent(http) {
+        this.http = http;
     }
+    AppComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        // grab users
+        this.http.get('https://reqres.in/api/users')
+            .subscribe(function (data) {
+            console.log(data.json());
+            _this.users = data.json().data;
+        });
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        styles: ["\n    \n  "],
-        template: "\n    <div class=\"jumbotron text-center\">\n      <h1>The App Lives!</h1>\n      <p>{{ message }}</p>\n    </div>\n  "
+        template: "\n    <div class=\"jumbotron text-center\">\n      <h1>The App Lives!</h1>\n      <p>{{ message }}</p>\n    </div>\n\n    <div *ngIf=\"users\">\n      <div *ngFor=\"let user of users\">\n        <h2>{{ user.first_name }}</h2>\n      </div>\n    </div>\n  "
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [http_1.Http])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
